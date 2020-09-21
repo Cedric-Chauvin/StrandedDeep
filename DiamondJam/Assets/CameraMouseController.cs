162e730219f83577d9a214feec4b662ab02a2c72
Matt <matthieu.manuellan@gmail.com>
2020-09-21 14:19:33 +0200

    FPS Controller

---
+++ DiamondJam/Assets/CameraMouseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseController : MonoBehaviour
+{
+
+    public float sensitivity = 100f;
+    private Transform _player;
+    private float _xRotation = 0;
+    private void Awake()
+    {
+    }
+    private void Start()
+    {
+        _player = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        Vector3 pRotation = Vector3.up * mouseX;
+        _player.Rotate(Vector3.up * mouseX);
+    }
+}
--- DiamondJam/Assets/PlayerController.cs
+++ DiamondJam/Assets/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 1.0f;
+    public float turnSmoothTime = 0.1f;
+    float _turnSmoothVelocity = 0.1f;
     void Update()
     {
+        Move();
+    }
+
+    public void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
         
+        Vector3 dir = (transform.right * x + transform.forward * z) * speed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x + dir.x, transform.position.y, transform.position.z + dir.z);
     }
 }
