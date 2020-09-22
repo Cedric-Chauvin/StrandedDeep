8589b3553211df314b1812515c19d9455cdf2265
Matt <matthieu.manuellan@gmail.com>
2020-09-22 12:19:28 +0200

    TriggerZoneEvent

---
+++ DiamondJam/Assets/Scripts/Events/TriggerEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : Event
+{
+    public TriggerZone zone;
+    private void Start()
+    {
+        type = EventType.TRIGGERZONE;
+    }
+
+    public override string BuildGameObjectName()
+    {
+        return "TriggerZone(" + zone.gameObject.name + "s)";
+    }
+
+    public override void LaunchEvent()
+    {
+        zone.canTrigger = true;
+    }
+}
---
+++ DiamondJam/Assets/TriggerZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public bool canTrigger = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && canTrigger)
+        {
+            canTrigger = false;
+            EventSequencer.Instance.NextEvent();
+        }
+    }
+}
